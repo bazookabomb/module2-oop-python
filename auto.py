f9093813b3dbfdaa3bb810b0a4c1269f4591027a
bazookabomb <riedel79mirko@gmail.com>
2025-04-29 15:16:34 +0200

    homework day 2

--- auto.py
+++ auto.py
@@ -1,12 +1,36 @@
-class MeineKlasse:
-    # Attribute / Eigenschaften
-    pass
-    # Methoden
-    def __init__(self):
-        # Konstruktor
-        pass
+# Beispiel für Magic Methoden
+class Auto:
+    ## Attribute
+    marke: str
+    modell: str
+    tankvolumen: int
 
-my_class = MeineKlasse()
-print(my_class)
-        
-        
+    def __init__(self, marke, modell, tankvolumen=80):
+        self.marke = marke
+        self.modell = modell
+        self.tankvolumen = tankvolumen
+
+    def __eq__(self, other):
+        return self.marke == other.marke and self.modell == other.modell
+    
+    def __str__(self):
+        return f"Der {self.modell} der Marke {self.marke} hat ein Tankvolumen von {self.tankvolumen} Liter"
+
+    def __repr__(self):
+        return f"Auto('{self.marke}', '{self.modell}', {self.tankvolumen})"
+
+mein_auto = Auto("VW", "Golf")
+anderes_auto = Auto("VW", "Passat")
+print(mein_auto) # ruft str-Methode auf
+print([mein_auto]) # ruft repr-Methoden auf
+# print(3 == 2)
+# print(f"Sind die Autos gleich? {mein_auto == anderes_auto}")
+
+# liste = [1,2]
+# liste[1] = 4
+
+# def main_function():
+#     print("Das ist die Hauptfunktion")
+
+# if __name__ == "__main__":
+#     main_function()
